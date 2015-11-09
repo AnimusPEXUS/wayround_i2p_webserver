de2c9514539a540c26ceb425c2f8ce4fcf3873bd
Alexey Gorshkov <animus@wayround.org>
2015-11-09 18:29:20 +0300

    fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='wayround_org_webserver',
-    version='0.1.2',
+    version='0.1.4',
     description='homebrewn webserver',
     author='Alexey Gorshkov',
     author_email='animus@wayround.org',
