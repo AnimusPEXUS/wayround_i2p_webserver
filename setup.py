aef5898efc5bdc9e422414bc631f08295730c04d
Alexey Gorshkov <animus@wayround.org>
2015-11-09 18:27:43 +0300

    fix

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='wayround_org_webserver',
-    version='0.1',
+    version='0.1.2',
     description='homebrewn webserver',
     author='Alexey Gorshkov',
     author_email='animus@wayround.org',
@@ -14,5 +14,10 @@ setup(
         ],
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
-        ]
+        ],
+    entry_points={
+        'console_scripts': [
+            'wrows = wayround_org.webserver.main'
+            ],
+        }
     )
--- wayround_org/webserver/commands.py
+++ wayround_org/webserver/commands.py
@@ -16,8 +16,8 @@ def commands():
 def server_run(command_name, opts, args, adds):
     ret = 0
     serv = wayround_org.webserver.webserver.WebServer(
-        #'/etc/wrows.conf'
-        '/home/agu/_local/p/wayround_org_webserver/trunk/test/wrows6.conf'
+        '/etc/wrows.conf'
+        #'/home/agu/_local/p/wayround_org_webserver/trunk/test/wrows6.conf'
         )
     serv.start()
     serv.wait_for_shutdown()
